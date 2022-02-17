060df10637e9021676df6bf78420c2d1a496a5ae
“lfy” <524414193@qq.com>
2022-02-17 20:07:06 +0800

    35%

--- route/register.js
+++ route/register.js
@@ -14,11 +14,19 @@ register.get('/register', async(req, res) => {
         result = await RegisterDB.find({}).skip((pageNum - 1) * pageSize).limit(pageSize - 0);
         result1 = await RegisterDB.find({ status: 0 }).skip((pageNum1 - 1) * pageSize1).limit(pageSize1 - 0);
     }
-    res.send({ result: { total, pageNum, registerList: result, total1, pageNum1, registerList1: result1 }, meta: { status: 200, des: 'Success' } });
+    res.send({ result: { total: total, pageNum: pageNum, registerList: result, total1: total1, pageNum1: pageNum1, registerList1: result1 }, meta: { status: 200, des: 'Success' } });
+    // res.send({ result: { total, pageNum, registerList: result }, meta: { status: 200, des: 'Success' } });
 });
 register.put('/insert', async(req, res) => {
     let result = await RegisterDB.insertMany(req.body);
     if (!result) return res.send({ result: null, meta: { status: 404, des: '入库信息创建失败' } });
     return res.send({ result: null, meta: { status: 200, des: '入库信息创建成功' } });
 });
+register.put('/update', async(req, res) => {
+    console.log(req.body);
+    let result = await RegisterDB.updateOne({ _id: req.body._id }, req.body);
+    console.log(result);
+    if (!result.acknowledged || !result.modifiedCount) return res.send({ result: null, meta: { status: 404, des: "材料出入库失败" } });
+    return res.send({ result: null, meta: { status: 200, des: "材料出入库成功" } })
+})
 module.exports = register
